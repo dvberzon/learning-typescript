6cf1218f0303620373b365134c138fc0870d7be3
Daniel Berzon <daniel@ocasta.com>
2018-12-18 17:43:39 +0000

    explicit interface

--- tslang/interfaces.ts
+++ tslang/interfaces.ts
@@ -1,9 +1,13 @@
 // https://www.typescriptlang.org/docs/handbook/interfaces.html
 
-function printLabel(labelledObj: { label: string }) {
+interface LabelledValue {
+  label: string;
+  name: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
   console.log(labelledObj.label);
 }
 
-let myObj = {size: 10, label: "Size 10 Object"};
-printLabel(myObj); // Note the duck typing. TS doesn't care if 'size' is there as an extra param
-
+let myObj = {name: 'henry', size: 10, label: "Size 10 Object"}; // note order doesn't matter
+printLabel(myObj);
