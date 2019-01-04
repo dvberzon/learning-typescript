208b545339d7676bcd98821aed4ac3ff4c02c33c
Daniel Berzon <daniel@ocasta.com>
2019-01-04 11:58:17 +0000

    vanilla create react app

---
+++ routing-with-ts/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <p>This is a react app, let's add ts</p>
+    );
+  }
+}
+
+export default App;
